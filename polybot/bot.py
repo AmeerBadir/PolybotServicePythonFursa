52d3ce6bc71f6739984a45a73b8aa14ba5ac9751
AmeerBadir <ameer.badir123@gmail.com>
2024-06-24 23:14:19 +0300

    update yaml

--- polybot/bot.py
+++ polybot/bot.py
@@ -100,7 +100,7 @@ class ImageProcessingBot(Bot):
             elif 'text' in msg and msg['text'].lower() == 'hi':
                 self.send_text(chat_id, "Hello, how can i help you?")
             elif 'text' in msg and msg['text'].lower() == 'thanks':
-                self.send_text(chat_id, "your welcome")
+                self.send_text(chat_id, "your welcomddde")
             elif 'text' in msg and msg['text'].lower() == 'done':
                 self.send_text(chat_id, "Good bye, we well be happy to see you again")
             else:
